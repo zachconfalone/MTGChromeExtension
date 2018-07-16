b0b589897d1939743c0dca826f538b3812548601
Zach <zachconfalone69@gmail.com>
2018-07-15 21:15:11 -0400

    Worked on setting different sets per the user choosing

--- popup.js
+++ popup.js
@@ -3,12 +3,14 @@ document.addEventListener('DOMContentLoaded',function(){
     document.getElementById("mybutton").addEventListener("click",handler);
     document.getElementById("moreInfo").addEventListener("click",moreInfoinit);
     document.getElementById("flipButton").addEventListener("click",flipArt);
+    document.getElementById("setPicker").addEventListener("click",selectArt);
 });
 
 var parsedJson;
 var divel = document.getElementById('cardPic');
 var cardFace1;
 var cardFace2;
+var setArray = {};
 
 //handle player input
 function handler()
@@ -18,12 +20,26 @@ function handler()
     var entCard = document.getElementById("EnteredCard").value;
     var returned = MakeRequest('https://api.scryfall.com/cards/named?fuzzy=' + entCard)
     parsedJson = JSON.parse(returned);
+    var setSearch = MakeRequest(parsedJson.prints_search_uri);
+    parsedSetJson = JSON.parse(setSearch);
+    //console.log(parsedSetJson.data[0]);
+    var counter = 0
+    for(counter = 0;counter < parsedSetJson.total_cards;counter++)
+    {
+        setArray[counter] = parsedSetJson.data[counter];
+    }
+    var setCounter = 0;
+    /*
+    for(setCounter = 0;setCounter<parsedSetJson.total_cards;setCounter++)
+    {
+        console.log(setArray[setCounter].set_name);
+    }
+    */
     if(parsedJson.status == 404)
     {
         alert('ALERT: Can not parse your request, please enter it again');
     }
     var cardName = parsedJson.name;
-    var oracle = parsedJson.oracle_text;
     for(each in parsedJson.legalities)
     {
         document.getElementById('legalities').textContent += each + ":" +parsedJson.legalities[each] + ' ' ;
@@ -33,12 +49,13 @@ function handler()
         document.getElementById("flipButton").style.visibility="hidden";
         var pictureURL = parsedJson.image_uris.normal
         document.getElementById("cardPic").src = pictureURL;
-        document.getElementById("cardInfo").innerHTML = "Card Name: " + cardName;
+        document.getElementById("cardInfo").innerHTML =  cardName;
     }
     if(parsedJson.layout == 'transform')
     {
         cardFace1 = parsedJson.card_faces[0].image_uris.normal;
         cardFace2 = parsedJson.card_faces[1].image_uris.normal;
+        document.getElementById("cardInfo").innerHTML = cardName;
         document.getElementById("cardPic").src = cardFace1;
         document.getElementById("flipButton").style.visibility = "visible";
 
@@ -71,6 +88,9 @@ function flipArt(){
             document.getElementById("cardPic").src = cardFace1;
             return;
     }
+}
 
-
+function selectArt(){
+    console.log(setArray[2].image_uris.normal);
+    document.getElementById("cardPic").src = setArray[2].image_uris.normal;
 }
